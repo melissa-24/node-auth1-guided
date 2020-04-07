53962a2e1b01e52f64bab4bfb764a343cbdbec0c
Sean Kirkby <sean.kirkby@niceincontact.com>
2020-04-07 16:34:49 -0600

    sync with lambdaschool

--- users/users-model.js
+++ users/users-model.js
@@ -1,4 +1,4 @@
-const db = require('../database/dbConfig.js');
+const db = require("../database/dbConfig.js");
 
 module.exports = {
   add,
@@ -8,24 +8,21 @@ module.exports = {
 };
 
 function find() {
-  return db('users').select('id', 'username', 'password');
+  return db("users").select("id", "username");
 }
 
 function findBy(filter) {
-  return db('users').where(filter);
+  return db("users").where(filter);
 }
 
-function add(user) {
-  return db('users')
-    .insert(user, 'id')
-    .then(ids => {
-      const [id] = ids;
-      return findById(id);
-    });
+async function add(user) {
+  const [id] = await db("users").insert(user, "id");
+
+  return findById(id);
 }
 
 function findById(id) {
-  return db('users')
+  return db("users")
     .where({ id })
     .first();
 }
